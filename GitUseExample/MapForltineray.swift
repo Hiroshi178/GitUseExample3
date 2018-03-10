5e1eaa7ca76dfc4c9f8414e7b96dfa61e22e8e79
Hiroshi178 <GBD02500@nifty.ne.jp>
2018-03-10 09:28:15 +0900

    Add new map feature

---
+++ GitUseExample/MapForltineray.swift
@@ -0,0 +1,13 @@
+//
+//  MapForltineray.swift
+//  GitUseExample
+//
+//  Created by 坂上弘 on 2018/03/10.
+//  Copyright © 2018年 坂上弘. All rights reserved.
+//
+
+import UIKit
+
+class MapForltineray: NSObject {
+
+}
